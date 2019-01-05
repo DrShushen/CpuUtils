c07d19977dd57979575ea119f77c0c8c42a451d0
Evgeny Saveliev <e.s.saveliev@gmail.com>
2019-01-05 00:27:25 +0000

    Initial commit.

---
+++ CpuUtils/CpuUtils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EvgenySaveliev.Utils
+{
+    /// <summary>
+    /// <para>A static CPU Utilities class that allows getting the number of CPU physical and logical cores available.</para>
+    /// <para>SystemException is thrown if for any reason ManagementObjectSearcher("Select NumberOfCores from Win32_Processor") query returns zero physical cores.</para>
+    /// </summary>
+    /// <exception cref="SystemException"></exception>
+    public class CpuUtils
+    {
+        static CpuUtils()
+        {
+            ObtainPhysicalCoreCount();
+            ObtainLogicalCoreCount();
+        }
+
+        /// <summary>
+        /// Gets the number of physical CPU cores available.
+        /// </summary>
+        public static int PhysicalCoreCount
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Gets the number of logical CPU cores available.
+        /// </summary>
+        public static int LogicalCoreCount
+        {
+            get; private set;
+        }
+
+        private static void ObtainPhysicalCoreCount()
+        {
+            int coreCount = 0;
+
+            foreach (var item in new System.Management.ManagementObjectSearcher("Select NumberOfCores from Win32_Processor").Get())
+            {
+                coreCount += int.Parse(item["NumberOfCores"].ToString());
+            }
+
+            if (coreCount == 0)
+                throw new SystemException("Couldn't get physical core count.");
+
+            PhysicalCoreCount = coreCount;
+        }
+
+        private static void ObtainLogicalCoreCount()
+        {
+            LogicalCoreCount = Environment.ProcessorCount;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+## Synopsis
+A small static utility class for getting the available physical and logical CPU cores on the system (C#).
+
+## Usage
+```csharp
+using EvgenySaveliev.Utils;
+
+...
+
+int physicalCoreCount = CpuUtils.PhysicalCoreCount;  // Get physical CPU core count.
+int logicalCoreCount = CpuUtils.LogicalCoreCount;  // Get logical CPU core count.
+```
